c805a8f7bbfb316b5a21bf7c73d38e780a8472a4
Emelie Fäldt <emeliefaldt@hotmail.com>
2026-03-17 15:51:17 +0100

    Project setup and Book entity created

---
+++ crud-web-app/src/main/java/model/Book.java
@@ -0,0 +1,74 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.time.LocalDate;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    @PastOrPresent
+    private LocalDate releaseDate;
+
+    @NotBlank
+    private String author;
+
+    @NotBlank
+    private String isbn;
+
+    // Getters och setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
