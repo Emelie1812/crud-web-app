7fbafc37a4c07670b4f1e17811c92ff6c106b93d
Emelie Fäldt <emeliefaldt@hotmail.com>
2026-03-17 16:10:18 +0100

    Added BookService layer

---
+++ crud-web-app/src/main/java/com/example/crud_web_app/service/BookService.java
@@ -0,0 +1,38 @@
+package com.example.crud_web_app.service;
+
+import com.example.crud_web_app.model.Book;
+import com.example.crud_web_app.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository repository;
+
+    public BookService(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    // Hämta alla böcker
+    public List<Book> getAllBooks() {
+        return repository.findAll();
+    }
+
+    // Spara bok
+    public void saveBook(Book book) {
+        repository.save(book);
+    }
+
+    // Hämta bok via id
+    public Book getById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+    }
+
+    // Ta bort bok
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
